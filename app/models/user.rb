3fe79e5332958e62596d94a99ca200e2e49214a3
marmite334 <68127550+marmite334@users.noreply.github.com>
2020-12-21 00:26:21 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :nickname, presence:true
-  validates :first_name, presence:true
-  validates :last_name, presence:true
-  validates :kana_first_name, presence:true
-  validates :kana_last_name, presence:true
+  validates :password, format: { with: /[a-z\d]{6,}/i, message: 'Password Include both letters and numbers' }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'First name Full-width characters' }
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'Last name Full-width characters' }
+  validates :kana_first_name, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'Kana first name Full-width katakana characters' }
+  validates :kana_last_name, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'Kana last name Full-width katakana characters' }
   validates :birth_date, presence:true
 end
