70219f1b2a685b3b4faba54959d3175b2027f86f
marmite334 <68127550+marmite334@users.noreply.github.com>
2020-12-27 00:51:37 +0900

    passwordのfactoryを英数字に変更#2

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname              { Faker::Name.initials(number: 2) }
     email                 { Faker::Internet.free_email }
-    password              { Faker::Alphanumeric.alphanumeric(number: 6) }
+    password              { Faker::Alphanumeric.alphanumeric(number: 6, min_alpha: 1, min_numeric: 1) }
     password_confirmation { password }
     last_name             { Gimei.last.kanji }
     first_name            { Gimei.first.kanji }
