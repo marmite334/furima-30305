91b367c5dbb13f48ca7c9e6e22541925c308e49b
marmite334 <68127550+marmite334@users.noreply.github.com>
2020-12-28 06:42:15 +0900

    createアクションの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,6 +6,15 @@ class ItemsController < ApplicationController
     @item = Item.new
   end
 
+  def create
+    Item.create(item_params)
+    if @item.save
+      redirect_to :action: :index
+    else
+      render :new
+    end
+  end
+
   private
   def item_params
     params.require(:item).parmit(:name, :explanation, :price, :category_id, :state_id, :charges_id, :prefecture_id, :days_id, :image).merge(user_id: current_user.id)
