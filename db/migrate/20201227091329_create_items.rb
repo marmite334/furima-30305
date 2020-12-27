a2dad6cffebf0fe26e6770d660de5b3e4468ad28
marmite334 <68127550+marmite334@users.noreply.github.com>
2020-12-27 18:32:18 +0900

    マイグレーションファイルの編集

--- db/migrate/20201227091329_create_items.rb
+++ db/migrate/20201227091329_create_items.rb
@@ -1,8 +1,15 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
-
-      t.timestamps
+      t.string     :name,          null: false
+      t.text       :explanation,   null: false
+      t.integer    :price,         null: false
+      t.integer    :category_id,   null: false
+      t.integer    :state_id,      null: false
+      t.integer    :charges_id,    null: false
+      t.integer    :prefecture_id, null: false
+      t.integer    :days_id,       null: false
+      t.references :user,          null: false, foreign_key: true
     end
   end
 end
