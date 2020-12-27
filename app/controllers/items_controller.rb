ec6a15e13f8aa431f65cc4eb874b6a3f4636a725
marmite334 <68127550+marmite334@users.noreply.github.com>
2020-12-28 06:31:07 +0900

    ストロングパラメータの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,4 +6,8 @@ class ItemsController < ApplicationController
     @item = Item.new
   end
 
+  private
+  def item_params
+    params.require(:item).parmit(:name, :explanation, :price, :category_id, :state_id, :charges_id, :prefecture_id, :days_id, :image).merge(user_id: current_user.id)
+  end
 end
