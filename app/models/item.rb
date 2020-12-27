59d645a8b2ee1c41953a045ce71828c71e302e64
marmite334 <68127550+marmite334@users.noreply.github.com>
2020-12-28 00:25:32 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,12 @@
 class Item < ApplicationRecord
-  belongs_to :user
+  validates       :name,          presence: true
+  validates       :explanation,   presence: true
+  validates       :price,         presence: true
+  validates       :category_id,   presence: true
+  validates       :state_id,      presence: true
+  validates       :charges_id,    presence: true
+  validates       :prefecture_id, presence: true
+  validates       :days_id,       presence: true
+  belongs_to      :user
+  has_on_attached :image
 end
