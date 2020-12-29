fcdb0cf8837b1ece76fdd7a29054dafde30da4e2
marmite334 <68127550+marmite334@users.noreply.github.com>
2020-12-30 00:55:32 +0900

    ActiveHashのエラーメッセージの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,13 +2,17 @@ class Item < ApplicationRecord
   validates        :name,          presence: true
   validates        :explanation,   presence: true
   validates        :price,         presence: true
-  validates        :category_id, numericality: { other_than: 1 } 
-  validates        :state_id,      presence: true
-  validates        :charges_id,    presence: true
-  validates        :prefecture_id, presence: true
-  validates        :days_id,       presence: true
+  validates        :category_id,   numericality: { other_than: 1 ,message: 'Select'} 
+  validates        :state_id,      numericality: { other_than: 1 ,message: 'Select'} 
+  validates        :charges_id,    numericality: { other_than: 1 ,message: 'Select'} 
+  validates        :prefecture_id, numericality: { other_than: 1 ,message: 'Select'} 
+  validates        :days_id,       numericality: { other_than: 1 ,message: 'Select'} 
   belongs_to       :user
+  has_one_attached :image
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to       :category
-  has_one_attached :image
+  belongs_to       :state
+  belongs_to       :charge
+  belongs_to       :prefecture
+  belongs_to       :days  
 end
