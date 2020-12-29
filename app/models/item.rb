5662269725158b21b1b086e6e3c4a048878b8514
marmite334 <68127550+marmite334@users.noreply.github.com>
2020-12-30 01:09:10 +0900

    金額のバリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,12 +1,14 @@
 class Item < ApplicationRecord
   validates        :name,          presence: true
   validates        :explanation,   presence: true
-  validates        :price,         presence: true
-  validates        :category_id,   numericality: { other_than: 1 ,message: 'Select'} 
-  validates        :state_id,      numericality: { other_than: 1 ,message: 'Select'} 
-  validates        :charges_id,    numericality: { other_than: 1 ,message: 'Select'} 
-  validates        :prefecture_id, numericality: { other_than: 1 ,message: 'Select'} 
-  validates        :days_id,       numericality: { other_than: 1 ,message: 'Select'} 
+  validates        :price,         presence: true, 
+                                   length:       { in: 300..9999999,   message: 'Out of setting range' },
+                                   format:       { with: /\A[0-9]+\z/, message: 'Half-width number'}
+  validates        :category_id,   numericality: { other_than: 1 ,     message: 'Select'} 
+  validates        :state_id,      numericality: { other_than: 1 ,     message: 'Select'} 
+  validates        :charges_id,    numericality: { other_than: 1 ,     message: 'Select'} 
+  validates        :prefecture_id, numericality: { other_than: 1 ,     message: 'Select'} 
+  validates        :days_id,       numericality: { other_than: 1 ,     message: 'Select'} 
   belongs_to       :user
   has_one_attached :image
   extend ActiveHash::Associations::ActiveRecordExtensions
