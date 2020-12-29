92e1d68cd574cb29ec3e649b29e8de3f662e8707
marmite334 <68127550+marmite334@users.noreply.github.com>
2020-12-29 15:49:34 +0900

    categoryモデルの作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,12 +1,14 @@
 class Item < ApplicationRecord
-  validates       :name,          presence: true
-  validates       :explanation,   presence: true
-  validates       :price,         presence: true
-  validates       :category_id,   presence: true
-  validates       :state_id,      presence: true
-  validates       :charges_id,    presence: true
-  validates       :prefecture_id, presence: true
-  validates       :days_id,       presence: true
-  belongs_to      :user
+  validates        :name,          presence: true
+  validates        :explanation,   presence: true
+  validates        :price,         presence: true
+  validates        :category_id, numericality: { other_than: 1 } 
+  validates        :state_id,      presence: true
+  validates        :charges_id,    presence: true
+  validates        :prefecture_id, presence: true
+  validates        :days_id,       presence: true
+  belongs_to       :user
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to       :category
   has_one_attached :image
 end
