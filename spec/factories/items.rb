cd2d5db9ac620f99dc382460a5f822e8ccaa9323
marmite334 <68127550+marmite334@users.noreply.github.com>
2020-12-30 14:32:01 +0900

    itemモデルのfactoriesの記述

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :item do
-    
+    name          { Faker::Commerce.department(max: 40)       }
+    explanation   { Faker::Lorem.sentence                     }
+    price         { Faker::Number.within(range: 300..9999999) }
+    category_id   { Faker::Number.within(range: 2..11)        }
+    state_id      { Faker::Number.within(range: 2..7)         }
+    charges_id    { Faker::Number.within(range: 2..3)         }
+    prefecture_id { Faker::Number.within(range: 2..48)        }
+    days_id       { Faker::Number.within(range: 2..4)         }
+    association :user
   end
 end
