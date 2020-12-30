bc04e9a2a705552223e6cd445cfcb66db8fa81aa
marmite334 <68127550+marmite334@users.noreply.github.com>
2020-12-30 18:09:54 +0900

    itemモデルpresence: trueをまとめる

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,14 +1,15 @@
 class Item < ApplicationRecord
-  validates        :name,          presence: true
-  validates        :explanation,   presence: true
-  validates        :price,         presence: true,
-                                   format: { with: /\A([1-9]\d*)*[1-9]\d*\z/, message: 'Half-width number' },
-                                   inclusion: { in: 300..9_999_999,   message: 'Out of setting range' }
-  validates        :category_id,   numericality: { other_than: 1,     message: 'Select' }
-  validates        :state_id,      numericality: { other_than: 1,     message: 'Select' }
-  validates        :charges_id,    numericality: { other_than: 1,     message: 'Select' }
-  validates        :prefecture_id, numericality: { other_than: 1,     message: 'Select' }
-  validates        :days_id,       numericality: { other_than: 1,     message: 'Select' }
+  with_options presence: true do
+    validates        :name
+    validates        :explanation
+    validates        :price,         format: { with: /\A([1-9]\d*)*[1-9]\d*\z/, message: 'Half-width number' },
+                                     inclusion: { in: 300..9_999_999,   message: 'Out of setting range' }
+    validates        :category_id,   numericality: { other_than: 1,     message: 'Select' }
+    validates        :state_id,      numericality: { other_than: 1,     message: 'Select' }
+    validates        :charges_id,    numericality: { other_than: 1,     message: 'Select' }
+    validates        :prefecture_id, numericality: { other_than: 1,     message: 'Select' }
+    validates        :days_id,       numericality: { other_than: 1,     message: 'Select' }
+  end
   belongs_to       :user
   has_one_attached :image
   extend ActiveHash::Associations::ActiveRecordExtensions
