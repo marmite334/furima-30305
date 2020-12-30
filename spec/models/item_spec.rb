13f2a208e08526ee0fd4011181e5569d2794f603
marmite334 <68127550+marmite334@users.noreply.github.com>
2020-12-30 15:18:25 +0900

    テスト画像の配置

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe '商品の保存' do
+    context '商品が保存できる場合' do
+      it 'imageとnameとexplanationとpriceとcategory_idとstate_idとcharges_idとprefecture_idとdays_idがあれば保存できる' do
+      end
+    end
+    context '商品が保存できない場合' do
+      it 
 end
