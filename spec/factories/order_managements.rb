0ab837fe2cbcd2fbc3c1ec0d27c06637141d2916
marmite334 <68127550+marmite334@users.noreply.github.com>
2021-01-18 21:31:42 +0900

    テストコードの記述

---
+++ spec/factories/order_managements.rb
@@ -0,0 +1,14 @@
+FactoryBot.define do
+  factory :order_management do
+    item_id                { 1 }
+    user_id                { 3 }
+    token                  { 'tok_abcdefghijk00000000000000000' }
+    postal_code            { '289-2114' }
+    prefecture_id          { 12 }
+    municipalities         { '匝瑳市'}
+    address                { '八日市場イ' }
+    building               { '八日市場駅' }
+    phone_number           { 11111111111 }
+    purchase_management_id { 1 }
+  end
+end
