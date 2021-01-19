cb6e39b6616833a2e42a7a6da142476c6de30ae8
marmite334 <68127550+marmite334@users.noreply.github.com>
2021-01-19 10:31:36 +0900

    トークン化処理

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,8 +1,25 @@
 const pay = () => {
+  Payjp.setPublicKey("");
   const form = document.getElementById("charge-form");
   form.addEventListener("submit", (e) => {
     e.preventDefault();
-    console.log("あああ")
+
+    const formResult = document.getElementById("charge-form");
+    const formData = new FormData(formResult);
+
+    const card = {
+      number: formData.get("order_management[number]"),
+      exp_month: formData.get("order_management[exp_month]"),
+      exp_year: `20${formData.get("order_management[exp_year]")}`,
+      cvc: formData.get("order_management[cvc]")
+    };
+
+    Payjp.createToken(card, (status, response) => {
+      if (status == 200) {
+        const token = response.id;
+        console.log(token)
+      }
+    });
   });
 };
 
